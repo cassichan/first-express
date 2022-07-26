416bbd0806cfc3c3207506c6775434c7dcce18c8
Cassandra Lee <cassichan@Cassandras-Air.attlocal.net>
2022-07-25 20:32:03 -0400

    updating notes

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 //Import express library
 import express from 'express';
 
-//Iniatalize express server
+//Initialize express server
 const app = express();
 //Tell express to accept data when sent to it in JSON format
 app.use(express.json());
 
 //List requests that we want express to respond to
-//If too many functions/routes, put functions elsewhere so code is neater.
 app.get('/hello', (request, response) => {
   //do something..ex: send response back through port
   console.log('xxxxHello Worldxxxx'); //Just to api, not browser
-  response.send('Hello World!');
+  response.send('Hello World!'); // to browser. -> status 200 implied
   //if you want to specify a status, include .status(). do this if not status 200
-  //response.send('Hello World!')  -> status 200 implied
 });
 
 //Array of objects (students). usually would be in DB or another file
@@ -35,13 +33,14 @@ const students = [
     lastName: 'McCalley',
   },
 ];
-
+//Get students. Converts students to JSON. if want to be explicit: response.json
 app.get('/students', (request, response) => {
-  response.send(students); //Converts students to JSON. if want to be explicit: response.json
+  response.send(students); 
 });
 
+//students/Mason
 app.get('/students/:fName', (request, response) => {
-    //Calling .find to return one student's first name in the array (stud = one element of students array. stud.firstName = single students firstName) that matches the request param firstName (fName). set to variable student
+    // (stud.firstName = single students firstName) that matches the request param firstName (fName)
     const student = students.find(stud => stud.firstName === request.params.fName)    
      //Generate response and send message back if student not found/success is false
     if(!student) {
@@ -53,13 +52,12 @@ app.get('/students/:fName', (request, response) => {
 }); //Good practice to include error as the if in the if statement
 
 
-//Make post request to students- add new student and return new student array
+//get a new student
 app.post('/students', (request, response) => {
     const newStudent = request.body;
     students.push(newStudent);
     response.status(201).send(students);
 });
-
 //Run Postman
 //Get request localhost:3001/students
 //Change to POST
@@ -74,7 +72,7 @@ app.post('/students', (request, response) => {
 // app.get('states/:stateName', returnStateByName)
 // app.get('states/:stateName', (req, res) => {returnStateByName (req, res)};
 
-//Start listening on a port. Give it a port and a callback to code what to do once listened to that port
+//Start listening on a port. Give it a port and a callback that says what to do once listened to that port
 
 app.listen(3001, () => {
   console.log('Now listening on port 3001');
